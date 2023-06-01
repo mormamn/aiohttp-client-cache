983bd6fb3c9075667186e2037db2a2560893222e
mormamn <61231073+mormamn@users.noreply.github.com>
2023-06-01 10:57:54 +0300

    Match ClientResponse
    Changed `close()` from `async def` to `def` to match ClientResponse;
    Added an empty __aexit__ so it can be used like ClientResponse with AsyncContextManager

--- aiohttp_client_cache/response.py
+++ aiohttp_client_cache/response.py
@@ -233,7 +233,13 @@ class CachedResponse(HeadersMixin):
     async def __aenter__(self) -> 'CachedResponse':
         return self
 
-    async def close(self):
+    async def __aexit__(self,
+                        exc_type: Optional[Type[BaseException]],
+                        exc_val: Optional[BaseException],
+                        exc_tb: Optional[TracebackType]) -> None:
+        pass
+    
+    def close(self):
         pass
 
     async def wait_for_close(self):
